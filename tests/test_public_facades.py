2933ed19a3a36fd9273790192f55b65d9a15445f
jason-wjs <jason-w@sjtu.edu.cn>
2026-06-01 14:35:15 +0800

    refactor: make legacy mujoco utils explicit aliases

--- tests/test_public_facades.py
+++ tests/test_public_facades.py
@@ -15,3 +15,12 @@ def test_visualization_playback_facade_preserves_legacy_helper_identity() -> Non
     from omniretarget.visualization.playback import create_motion_control_sliders as PlaybackHelper
 
     assert LegacyHelper is PlaybackHelper
+
+
+def test_legacy_mujoco_utils_reexports_asset_helpers() -> None:
+    from omniretarget.mujoco import assets
+    from omniretarget.src import mujoco_utils
+
+    assert mujoco_utils._mesh_local_vf is assets.mesh_local_vertices_and_faces
+    assert mujoco_utils._to_world is assets.transform_mesh_vertices_to_world
+    assert mujoco_utils._world_mesh_from_geom is assets.world_mesh_from_geom
